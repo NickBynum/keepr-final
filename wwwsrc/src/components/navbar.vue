bc9fb52f84acd6bf5d4054e333ec075016a0d9c9
Nick Bynum <exander@live.com>
2020-05-24 17:24:59 -0600

    added fluid container for mobile optimization

--- wwwsrc/src/components/navbar.vue
+++ wwwsrc/src/components/navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary container-fluid">
     <router-link class="navbar-brand" :to="{ name: 'home' }">Keepr</router-link>
     <button
       class="navbar-toggler"
