5ea3e534973f739dbbf658fef3c0764c6029b2dc
Nick Bynum <exander@live.com>
2020-05-31 20:16:07 -0600

    init vault view

---
+++ wwwsrc/src/views/Vault.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="vault">
+
+
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'vault',
+  data(){
+    return {}
+  },
+  computed:{},
+  methods:{},
+  components:{}
+}
+</script>
+
+
+<style scoped>
+
+</style>
